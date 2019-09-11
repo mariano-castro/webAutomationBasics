111bdae4767bbdec295c28650935143417a16642
Diego Muñoz <diego.md09@gmail.com>
2019-09-11 12:20:43 -0300

    WebElement y FindBy

--- src/main/java/com/automation/BasePage.java
+++ src/main/java/com/automation/BasePage.java
@@ -1,7 +1,9 @@
 package com.automation;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
